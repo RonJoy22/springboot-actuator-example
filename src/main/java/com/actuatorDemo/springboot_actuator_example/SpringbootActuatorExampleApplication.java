1a1781019cf14d35c71e5c8baa835351dfad8364
ronald.joyner-brown <r.brownjoyner@gmail.com>
2024-08-27 11:41:42 -0400

    Initial commit

---
+++ src/main/java/com/actuatorDemo/springboot_actuator_example/SpringbootActuatorExampleApplication.java
@@ -0,0 +1,13 @@
+package com.actuatorDemo.springboot_actuator_example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActuatorExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootActuatorExampleApplication.class, args);
+	}
+
+}
